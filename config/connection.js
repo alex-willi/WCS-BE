752037ecd8970bfb97b4f6aee98f80eea3e017e6
alex-willi <alex.lawson.williams@gmail.com>
2023-02-02 14:10:50 -0600

    changed to MONGODB_URI

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 
 const mongoose = require("mongoose");
-const { MONGO_URI } = process.env;
+const { MONGODB_URI } = process.env;
 
 mongoose.set("strictQuery", true);
-mongoose.connect(MONGO_URI);
-console.log(MONGO_URI);
+mongoose.connect(MONGODB_URI);
+
 mongoose.connection
   .on("open", () => console.log("You are connected to mongoose"))
   .on("close", () => console.log("You are disconnected from mongoose"))
