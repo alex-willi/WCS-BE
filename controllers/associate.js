ef1b6e503ef52b69ca4022ef381dd3b4c9b524a8
alex-willi <alex.lawson.williams@gmail.com>
2023-02-02 23:02:24 -0600

    update 404 error for get route

--- controllers/associate.js
+++ controllers/associate.js
@@ -32,9 +32,7 @@ router.get("/", requireToken, async (req, res, next) => {
     const associate = await Associate.findOne({ owner: req.user._id });
 
     if (!associate) {
-      return res
-        .status(404)
-        .json({ error: "Associate not found", owner: req.user._id });
+      return res.status(404).json({ message: "MAKE A PROFILE" });
     }
     res.status(200).json({ associate: associate, owner: owner });
   } catch (err) {
